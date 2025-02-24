3a008e4cca3d6febc15738b76168882d6f1bc92b
jibesh <jibeshshrestha00@gmail.com>
2025-02-24 14:20:30 +0545

    add mapreduce coordinator main function (entry point for coordinator process)

--- src/main/mrcoordinator.go
+++ src/main/mrcoordinator.go
@@ -1,36 +1,19 @@
 package main
 
-import "plugin"
+import "time"
 import "os"
 import "fmt"
-import "log"
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: mrworker xxx.so\n")
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: mrcoordinator inputfiles...\n")
 		os.Exit(1)
 	}
 
-	mapf, reducef := loadPlugin(os.Args[1])
-
-	Worker(mapf, reducef)
-}
-
-func loadPlugin(filename string) (func(string, string) []KeyValue, func(string, []string) string) {
-	p, err := plugin.Open(filename)
-	if err != nil {
-		log.Fatalf("cannot load plugin %v", filename)
+	m := MakeCoordinator(os.Args[1:], 10)
+	for m.Done() == false {
+		time.Sleep(time.Second)
 	}
-	xmapf, err := p.Lookup("Map")
-	if err != nil {
-		log.Fatalf("cannot find Map in %v", filename)
-	}
-	mapf := xmapf.(func(string, string) []KeyValue)
-	xreducef, err := p.Lookup("Reduce")
-	if err != nil {
-		log.Fatalf("cannot find Reduce in %v", filename)
-	}
-	reducef := xreducef.(func(string, []string) string)
 
-	return mapf, reducef
+	time.Sleep(time.Second)
 }
